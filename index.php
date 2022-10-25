ab13a6c611a2ff7b319e0ef1980b4626eec5f0d5
Aron <graph@livemail.tw>
2022-10-25 23:35:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<div id=main>
+
 <?php
 <meta http-equiv="refresh" content="180">
 $json = file_get_contents('https://api.henrikdev.xyz/valorant/v1/mmr/ap/kazaf/carry');
@@ -7,8 +7,7 @@ $rankbandge = $data->data->images->large;
 $rankName = $data->data->currenttierpatched;
 $rankNameC = strtoupper($rankName);
 $rankRR = $data->data->ranking_in_tier;
-echo '<div id=rb>'.'<img src='.$rankbandge.'/>' . '</div>';
-echo '<div id=ri>'. '<p>' . $rankNameC .'&ensp;-' .'</p>' .'</div>';
-echo '<div id=rRR>'. '<p>' .$rankRR .'ᴿᴿ'.'</p>' .'</div>';
+echo '<img src='.$rankbandge.'/>';
+echo $rankNameC .'&ensp;-';
+echo $rankRR .'ᴿᴿ';
 ?>
-</div>
