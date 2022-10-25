a64cee7fec1ad8e95005608b226c74be955953b6
Aron <graph@livemail.tw>
2022-10-25 23:33:47 +0800

    Update index.php

--- index.php
+++ index.php
@@ -7,43 +7,8 @@ $rankbandge = $data->data->images->large;
 $rankName = $data->data->currenttierpatched;
 $rankNameC = strtoupper($rankName);
 $rankRR = $data->data->ranking_in_tier;
-
-
 echo '<div id=rb>'.'<img src='.$rankbandge.'/>' . '</div>';
 echo '<div id=ri>'. '<p>' . $rankNameC .'&ensp;-' .'</p>' .'</div>';
 echo '<div id=rRR>'. '<p>' .$rankRR .'ᴿᴿ'.'</p>' .'</div>';
 ?>
 </div>
-<style>
-div#main {
-    display: flex;
-}
-div #rb {
-	width:200px;
-	height:200px;
-	}
-div #rb img{
-		width:150px;
-		height:150px;
-	}
-div #ri {
-	height:200px;
-	width:auto;
-	}
-div #ri p {
-    color: white;
-    margin: 35px;
-    margin-left: -41px;
-    font-size: 102px;
-}
-div #rRR {
-	text-align: center;
-	margin-left: 20px;
-	}
-div #rRR p{
-	font-size:107px;
-	margin:14px;
-	margin-left: -11px;
-	color:white;
-	}
-</style>
