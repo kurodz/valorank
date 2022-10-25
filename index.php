ab7874bc67cad09fc157c58a75198975616b615f
Aron <graph@livemail.tw>
2022-10-26 04:46:17 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@500&display=swap" rel="stylesheet">
+<div id=outer>
 <div id=main>
 <?php
 $json = file_get_contents('https://api.henrikdev.xyz/valorant/v1/mmr/ap/kazaf/carry');
@@ -9,24 +10,38 @@ $rankbandge = $data->data->images->large;
 $rankName = $data->data->currenttierpatched;
 $rankNameC = strtoupper($rankName);
 $rankRR = $data->data->ranking_in_tier;
+
 if($json === false){header("Refresh:0");}
 
-echo '<div id=rb>'.'<img src='.$rankbandge.'/>' . '</div>';
-echo '<div id=ri>'. '<p>' . $rankNameC .'&ensp;-' .'</p>' .'</div>';
 echo '<div id=rRR>'. '<p>' .$rankRR .'ᴿᴿ'.'</p>' .'</div>';
+echo '<div id=ri>'. '<p>' . '-&ensp;'. $rankNameC .'</p>' .'</div>';
+echo '<div id=rb>'.'<img src='.$rankbandge.'/>' . '</div>';
+
+
 ?>
+</div>
 </div>
 
 
 
 
 <style>
-
-
-div#main {
-    display: flex;
+body{
+	direction: rtl;
+}
+div #outer{
+	width:100%;
+	justify-content: center;
+}
+div #main {
+	width:100%;
+	display: flex;
+	margin-right: 2%;
+	justify-content: center;
 }
 div #rb {
+	display: flex;
+	margin-right: 3%;
 	width:200px;
 	height:200px;
 	}
@@ -35,8 +50,10 @@ div #rb img{
 		height:150px;
 	}
 div #ri {
+	display: flex;
 	height:200px;
 	width:auto;
+	white-space: nowrap;
 	}
 div #ri p {
     color: white;
@@ -46,6 +63,7 @@ div #ri p {
     font-size: 102px;
 }
 div #rRR {
+	display: flex;
 	text-align: center;
 	margin-left: 20px;
 	}
@@ -62,4 +80,3 @@ setTimeout(function(){
 window.location.reload(1);
 }, 180000);
 </script>
-	
